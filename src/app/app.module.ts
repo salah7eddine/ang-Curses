e21a9a08083f4d4d6e8da475bcd143ee76e0b491
ABOUSAID <salah-eddine.abousaid@capgemini.com>
2019-06-24 16:08:31 +0100

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { CoursesComponent } from './courses/courses.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
+import { ContentComponent } from './content/content.component';
+import { FormComponent } from './form/form.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CoursesComponent,
+    NavbarComponent,
+    SidebarComponent,
+    ContentComponent,
+    FormComponent
   ],
   imports: [
     BrowserModule
